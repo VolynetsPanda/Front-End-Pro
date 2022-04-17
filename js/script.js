5e84a44841d8e8b2a04a91dc4eaa6340b3c71875
evhenii-volynets <hw.volynets@gmail.com>
2022-04-17 17:20:36 +0300

    test

---
+++ js/script.js
@@ -0,0 +1,117 @@
+// *
+// **
+// ***
+// ****
+// var sum = ''
+// for (var i = 0; i < 4; i++){
+    // if(i == 0) sum = sum + '*' + '\n'
+    // if(i == 1) {
+    //     for (var j = 0; j < 2; j++){
+    //         sum = sum + '*'
+    //     }
+    //     sum = sum + '\n'
+    // }
+    // if(i == 2) {
+    //     for (var j = 0; j < 3; j++){
+    //         sum = sum + '*'
+    //     }
+    //     sum = sum + '\n'
+    // }
+    // if(i == 3) {
+    //     for (var j = 0; j < 4; j++){
+    //         sum = sum + '*'
+    //     }
+    //     sum = sum + '\n'
+    // }
+// }
+// console.log(sum)
+// var sum = '';
+// var str = '';
+// for (var i = 0; i < 4; i++){
+//     str = str + '*';
+//     // sum = sum + str + '\n'
+//     console.log(str + '\n')
+// }
+// console.log(sum)
+// str = '*' sum = '*\n';
+// str = '**' sum = '*\n**\n';
+// str = '***' sum = '*\n**\n***\n';
+
+//FUNCTION
+// var a = function (){
+//     console.log('Petya')
+// }
+// a()
+// var name = 'test'
+// function f(name, num, bool, age){
+//     // console.log(arguments)
+//     console.log(name)
+//     console.log(num + num)
+//     console.log(bool)
+//     console.log(age + age)
+// }
+// f('Vasya', true, 777);
+// f('ttt');
+// f('yyyyy');
+// function createUser(name, age, phone){
+//     var str = name + ", " + age + ", " + phone
+//     return str;
+// }
+// console.log(createUser('Vasya', 19, "+36........"))
+// function calc (d, op1, op2){
+//     return (d == "+") && op1 + op2;
+//     // if(d == "+") return op1 + op2;
+//     return op1 - op2;
+// }
+// console.log(
+//     calc(
+//         prompt('d'),
+//         +prompt(1),
+//         +prompt(2)
+//     )
+// )
+// var sum = ''
+// function addStr(count, row, i, sym){
+//     if(i == row){
+//         for (var j = 0; j < count; j++){
+//             sum = sum + sym
+//         }
+//         sum = sum + '\n'
+//     }
+// }
+// for (var i = 0; i < 4; i++){
+//     addStr(1, 0, i, "#")
+//     addStr(2, 1, i, "*")
+//     addStr(3, 2, i, "!")
+//     addStr(4, 3, i, "#")
+// }
+// console.log(sum)
+// var test1 = 1
+// var test3 = 'test'
+// function a() {
+//     var test2 = 2
+//     function b(num){
+//         var test3 = 3
+//         console.log(test1)
+//         console.log(test2)
+//         console.log(test3)
+//     }
+//     b()
+//     function c(num){}
+//     console.log(test1)
+//     console.log(test2)
+//     console.log(test3)
+// }
+// a()
+// console.log(a())
+// b()
+// var test1 = 1;
+// function a() {
+//     var b = 1;
+//     test1 += b;
+//     // return ++test1
+// }
+// a()
+// a()
+// a()
+// console.log(test1)
