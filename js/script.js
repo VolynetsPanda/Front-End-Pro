3df33e4e784160a5711f6e8f61169998f81afb1b
evhenii-volynets <hw.volynets@gmail.com>
2022-04-26 10:34:30 +0300

    array

---
+++ js/script.js
@@ -0,0 +1,97 @@
+// var test = {
+//     id: "4234232ew",
+//     name: 'Vasya',
+//     age: 100,
+//     user: {
+//         a: 'aaaa',
+//         b: 'bbbbb'
+//     },
+//     getInfo: function (name){
+//         return 'test ' + name
+//     }
+// }
+//
+// var a = {
+//     id: '4234232ew',
+//     name: 'Vasya',
+//     age: 100,
+//     user: {
+//         a: 'aaaa',
+//         b: 'bbbbb'
+//     },
+//     getInfo: function (name){
+//         return 'test ' + name
+//     }
+// }
+//
+// console.log(test.id == a.id)
+
+
+// var arr = [12, 4, true, null, 'Vasya']
+// arr[1] = 23;
+// delete arr[1];
+// console.log(arr)
+// for(var i = 0; i < arr.length; i++){
+//     console.log('i => ', i, 'arr => ', arr[i] * 10)
+// }
+
+// var arr = [23, 45, 10, 4, 100, false];
+
+// push
+// var test = arr.push(4234, 423, 'rer', [1, 2 ,3 ,4]);
+// console.log('arr => ', arr, 'test => ', test)
+
+// unshift
+// var test = arr.unshift(4234, 423, 'rer', [1, 2 ,3 ,4]);
+// console.log('arr => ', arr, 'test => ', test)
+
+// pop
+// var test = arr.pop();
+// console.log('arr => ', arr, 'test => ', test)
+
+// shift
+// var test = arr.shift();
+// console.log('arr => ', arr, 'test => ', test)
+
+// isArray
+// var test = Array.isArray(arr)
+// console.log(Array.prototype)
+
+// slice
+// var test = arr.slice(0)
+// console.log('arr => ', arr, 'test => ', test)
+
+// splice
+// var test = arr.splice(3, 1, 'yryr', 4323)
+// console.log('arr => ', arr, 'test => ', test)
+
+// indexOf
+// var test = arr.indexOf(10, 4)
+// console.log('arr => ', arr, 'test => ', test)
+
+// lastIndexOf
+// var test = arr.lastIndexOf(10, 4)
+// console.log('arr => ', arr, 'test => ', test)
+var arr = [ 45, 23, 10, 4, 100, 10, false, 'vasya', 'fedya', 'petya'];
+// sort
+// arr.sort(function (a, b){
+//     // console.log(a, b)
+//     // if(a > b) return -1
+//     // if(a === b) return 0
+//     // if(a < b) return 1
+//     return b - a
+// })
+// console.log(arr)
+
+// reverse
+// var test = arr.reverse()
+// console.log('arr => ', arr, 'test => ', test)
+
+// includes
+// var test = arr.includes('dss')
+// console.log('arr => ', arr, 'test => ', test)
+
+
+
+
+
