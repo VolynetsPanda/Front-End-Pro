d920c6bab02c0c02322c92de736b7b24a331d2d3
evhenii-volynets <hw.volynets@gmail.com>
2022-05-17 11:02:54 +0300

    dom (part 2)

--- js/dom.js
+++ js/dom.js
@@ -14,12 +14,91 @@ var body = document.body
 // var prev = body.previousElementSibling
 // var next = body.nextElementSibling
 // var parent = body.parentElement
+// var elems = document.querySelector('span')
 // var elems = document.querySelectorAll('.list-link')
-// var elems = document.querySelector('.list-link')
 // var elems = document.getElementsByTagName('li')
 // var elems = document.getElementsByName('fdfd')
-
-console.log(elems)
-
-
-
+// var test = elems.querySelectorAll('span')
+// var testDiv = elems.closest('.test-div-2')
+// var elems = document.querySelectorAll('.list-link')
+// elems.forEach(function (elem) {
+//     if (elem.matches('[href="#2"]')){
+//         console.log(elem, 'active')
+//     } else {
+//         console.log(elem, 'no active')
+//     }
+// })
+// var elems = document.querySelector('h1')
+// var title = elems.innerHTML
+// var title = elems.outerHTML
+// var str = ' ,<span>Vasya!!!</span>,'
+// elems.innerHTML = title.split(',').join(str)
+// var title = elems.textContent
+// elems.textContent += str
+// var elems = document.querySelectorAll('.list-link')
+// elems.forEach(function (elem) {
+//     var check = elem.matches('[href="#2"]')
+//     elem.textContent = check ? 'active' : 'no active'
+//     // if (elem.matches('[href="#2"]')){
+//     //     elems[i].textContent = 'active'
+//     // } else {
+//     //     elems[i].textContent = 'no active'
+//     // }
+// })
+// console.log(elems)
+// var div = document.createElement('div')
+// var list = document.querySelector('#list')
+// div.innerHTML = list.outerHTML
+// list.before(div)
+// document.querySelector('.test-div-------').after(div)
+// list.prepend(div)
+// list.append(div)
+// var text = document.querySelector('h1')
+// var list = document.querySelector('#list')
+// var clone = text.cloneNode(true)
+// list.append(clone)
+// text.remove()
+// var elems = document.querySelector('.list-link')
+// var className = elems.className
+// elems.className += ' vasya'
+// console.log(className)
+// elems.classList.add('vasya')
+// elems.classList.remove('tesing')
+// var elems = document.querySelectorAll('.list-link')
+// elems.forEach(function (elem) {
+    // elem.classList.toggle('tesing')
+    // var check = elem.classList.contains('tesing')
+    // if (check){
+    //     elem.classList.add('yellow')
+    //     console.log(elem, 'active')
+    // } else {
+    //     console.log(elem, 'no active')
+    // }
+// })
+// var div = document.createElement('div')
+// div.classList.add('testing')
+// div.className = 'Testing active vasya'
+// function px(num){
+//     return num + 'px'
+// }
+// var elem = document.querySelector('.list-link')
+// var input = document.querySelector('input')
+// elem.style.color = 'black'
+// elem.style.marginLeft = px(20)
+// elem.style.cssText = '' +
+//     'margin-left: 20px;' +
+//     'color: black'
+// var size = elem.style.fontSize // error
+// var style = getComputedStyle(elem, "::before")
+// var style = getComputedStyle(elem)
+// console.log(parseInt(style.fontSize))
+// var link = elem.href
+// input.value = 'vasya'
+// console.log(link)
+// console.log(value)
+// var check = input.hasAttribute('value')
+// var value = input.getAttribute('data-id')
+// var value = input.dataset
+// input.setAttribute('value', 'safsa')
+// input.removeAttribute('value')
+// console.log(value.id)
