742655ca015d09ee65ea4a5df1c64ae36cffc8cb
evhenii-volynets <hw.volynets@gmail.com>
2022-05-23 21:17:51 +0300

    part 2

--- js/script.js
+++ js/script.js
@@ -76,39 +76,116 @@
 //         block.hidden = true
 //     }
 // })
-var fields = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
-var col = 0
-function createBtn(tag, clName, text){
-    var btn = document.createElement(tag);
-    btn.className = clName;
-    btn.innerHTML = text;
-    btn.setAttribute('data-id', text)
-    var wrap = document.querySelector('.wrap');
-    wrap.append(btn)
-}
-for (var i = 0; i<fields.length; i++) {
-    for(var j = 0; j<fields.length; j++) {
-        createBtn('button', 'btn', `${j + 1}-${fields[i]} (${col})`)
-        col++;
+// var fields = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+// var col = 0
+// function createBtn(tag, clName, text){
+//     var btn = document.createElement(tag);
+//     btn.className = clName;
+//     btn.innerHTML = text;
+//     btn.setAttribute('data-id', text)
+//     var wrap = document.querySelector('.wrap');
+//     wrap.append(btn)
+// }
+// for (var i = 0; i<fields.length; i++) {
+//     for(var j = 0; j<fields.length; j++) {
+//         createBtn('button', 'btn', `${j + 1}-${fields[i]} (${col})`)
+//         col++;
+//     }
+// }
+// var wrap = document.querySelector('.wrap');
+// document.addEventListener('click', function (e){
+//     var elem = e.target;
+//     var check = elem.matches('.btn');
+//     if (check) {
+//         console.log(elem.dataset.id)
+//     }
+// })
+// var btn = document.querySelector('.btn');
+// btn.addEventListener('mousedown', function (e){
+//     console.log('mousedown', e)
+// })
+// btn.addEventListener('mouseup', function (e){
+//     console.log('mouseup', e)
+// })
+// btn.addEventListener('click', function (e){
+//     console.log('click', e)
+// })
+// btn.addEventListener('contextmenu', function (e){
+//     console.log('contextmenu', e)
+// })
+// var block = document.querySelector('.block');
+// // var wrap = document.querySelector('.wrap');
+// block.addEventListener('mouseover', function (e){
+//     var target = e.target.closest('span');
+//     console.log(target)
+//     if (!target) return;
+//     target.style.cssText = 'background: red;'
+// })
+// block.addEventListener('mouseout', function (e){
+//     var target = e.target.closest('span');
+//     if (!target) return;
+//     target.style.cssText = 'background: orange;'
+// })
+// var block = document.querySelector('.block');
+// var x = document.querySelector('.x');
+// var y = document.querySelector('.y');
+// var opt = {
+//     move: false,
+//     down: false,
+//     up: false
+// }
+// block.addEventListener('mousemove', function (e){
+//     var target = e.target.closest('span');
+//     if(target && opt.down){
+//         target.style.left = e.clientX - (target.offsetWidth / 2) + 'px';
+//         target.style.top = e.clientY - (target.offsetHeight / 2) + 'px';
+//     }
+//     x.innerHTML = e.clientX
+//     y.innerHTML = e.clientY
+//     // x.innerHTML = e.pageX
+//     // y.innerHTML = e.pageY
+// })
+// block.addEventListener('mousedown', function (e){
+//     var target = e.target.closest('span');
+//     if(target){
+//         opt.down = true
+//         target.style.position = 'fixed'
+//     }
+// })
+// block.addEventListener('mouseup', function (e){
+//     opt.down = false
+// })
+// var block = document.querySelector('.block');
+// console.log(block.getBoundingClientRect())
+// console.log(window.innerHeight)
+// document.addEventListener('keydown', function (e){
+//     console.log(e.code, " - ", e.key)
+// })
+// var count = document.querySelector('.count')
+// var text = document.querySelector('.text')
+// document.addEventListener('keyup', setTextCount)
+// document.addEventListener('keydown', function (e){
+//     if(e.repeat) setTextCount()
+// })
+//
+// function setTextCount(){
+//     count.innerHTML = text.value.length
+// }
+var step = 10;
+document.addEventListener('keydown', function (e){
+    var car = document.querySelector('.car');
+    var wCar = car.getBoundingClientRect().width;
+    var lCar = car.getBoundingClientRect().left;
+    var tCar = car.getBoundingClientRect().top;
+    if(e.code === "KeyD"){
+        console.log(window.innerWidth, lCar)
+        car.style.left = lCar >= window.innerWidth - wCar - step
+            ? window.innerWidth - wCar + 'px'
+            : lCar + step + 'px'
+        car.style.transform = 'rotate(180deg)';
+    } else if(e.code === "KeyA"){
+        car.style.left = lCar < 0 ? 0 + 'px' : lCar - step + 'px'
+        car.style.transform = 'rotate(0deg)';
     }
-}
-var wrap = document.querySelector('.wrap');
-document.addEventListener('click', function (e){
-    var elem = e.target;
-    var check = elem.matches('.btn');
-    if (check) {
-        console.log(elem.dataset.id)
-    }
+
 })
-
-
-
-
-
-
-
-
-
-
-
-
