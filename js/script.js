70ea92855b5aa6c26b8b630b625e357eafa3697e
evhenii-volynets <hw.volynets@gmail.com>
2022-07-04 20:51:07 +0300

    promise

--- js/script.js
+++ js/script.js
@@ -1,109 +1,49 @@
-// const xhr = new XMLHttpRequest();
-const status = {
-  '200': true,
-  '201': true,
-  '203': true
-}
-const pagination = {
-    limit: 20,
-    offset: 20,
-    page: 1
-}
-// let users = []
-let url = 'https://pokeapi.co/api/v2/'
-// xhr.open('GET', `${url}pokemon`)
-// //-------- POST ------
-// // let user = {
-// //     name: 'Vasya',
-// // }
-// // let formData = new FormData(user);
-// // formData.append('name', 'Vasya')
-// // xhr.setRequestHeader('X-Auth', 'Bearer ghfdgh')
-// // xhr.setRequestHeader('Content-Type', 'application/json;')
-// //--------------
-// xhr.send()
-// // xhr.send(JSON.stringify(formData))
-const wb = document.querySelector('.wrap-btn')
-const info = document.querySelector('.info')
-const link = document.querySelectorAll('.pagination')
-const createElem = (tag, append, src = null, text = null, api = null) => {
-    let elem = document.createElement(tag)
-    if(src){
-        elem.src = src
-        elem.alt = text
-        elem.title = text
-    }
-    if(!src && text){
-        elem.innerHTML = text
-    }
-    if(api){
-        elem.addEventListener('click', () => {
-            fetch(api)
-                .then(res => res.json())
-                .then(res => {
-                    console.log(res)
-                    const { sprites = {}, species = {} } = res
-                    const { front_default } = sprites
-                    const { name  } = species
-                    console.log(front_default)
-                    info.innerHTML = ''
-                    createElem('h3', info, null, name)
-                    createElem('img', info, front_default, name)
-                })
-        })
-    }
+let url = 'http://gateway.marvel.com/v1/public/comics'
+// let ts = Date.now()
+let ts = '1656951848714'
+let apikey = 'bb5bb3476d24cc359afcce18e800391e'
+let hash = 'f3cab8e90d178f805aa4c07c69f1b3a0'
+let api = `${url}?ts=${ts}&apikey=${apikey}&hash=${hash}0`
+let token = 'zm6qal8nzx7t6824ayngio6nl4agydwhgd52gp38'
+let url2 = 'https://api.json-generator.com/templates/Z9xhHIPuW35L/data'
+let api2 = `${url2}?access_token=${token}`
 
-    append.append(elem)
-}
-// const render = (arr) => {
-//     users = arr
-//     arr.forEach(el => {
-//         createElem('p', document.body, null, el.name)
+// fetch(api)
+//     .then(arg => arg.json())
+//     .then(res0 => {
+//         console.log("testng json 0 => ", res0)
+//         fetch('https://cat-fact.herokuapp.com/facts?animal_type=cat')
+//             .then(res => res.json())
+//             .then(json1 => {
+//                 console.log("testng json 1 => ", json1)
+//                 fetch(api2)
+//                     .then(res => res.json())
+//                     .then(json2 => {
+//                         console.log("testng json 2 => ", json2)
+//                         fetch('../generated.json')
+//                             .then(res=>res.json())
+//                             .then(json3 => {
+//                                 console.log("testng json 3 => ", json3)
+//                                 console.log("final => ", [res0, json1, json2, json3])
+//                             })
+//                     })
+//             })
 //     })
-//     console.log(users)
-// }
-// xhr.onload = function (){
-//     if(status[xhr.status]){
-//         let data = JSON.parse(xhr.response)
-//         let { results } = data
-//         render(results)
-//         // users = results
-//         results.forEach(el => {
-//             createElem('p', document.body, null, el.name)
-//         })
-//         // console.log(results)
-//     } else {
-//         console.log('error', xhr.response)
-//     }
-// }
-// setTimeout(() => {
-//     console.log(users)
-// }, 2000)
-const apiRequest = () => {
-    let customLink = `?limit=${pagination.limit}&offset=${pagination.offset * pagination.page}`
-    fetch(`${url}pokemon${customLink}`)
-        // .then( async res => {
-        //     const { results } = await res.json()
-        //     results.forEach(el => {
-        //         createElem('p', document.body, null, el.name)
-        //     })
-        //     console.log(results)
-        // })
-        .then(res => res.json())
-        .then(res => {
-            const {results} = res
-            results.forEach(el => {
-                createElem('button', wb, null, el.name, el.url)
-            })
-            console.log(results)
-        })
-}
-link.forEach(elem => {
-    elem.addEventListener('click', function (e){
-        e.preventDefault()
-        wb.innerHTML = ''
-        pagination.page = elem.getAttribute('data-id')
-        apiRequest()
-    })
-})
-apiRequest()
+
+const a1 = new Promise( resolve => resolve(fetch(api)
+    .then(arg => arg.json())))
+    // .then(res0 => console.log("testng json 0 => ", res0))
+
+const a2 = () => fetch('https://cat-fact.herokuapp.com/facts?animal_type=cat')
+    .then(res=>res.json())
+    // .then(json => console.log("testng json 1 => ", json))
+
+const a3 = () => fetch(api2)
+    .then(res=>res.json())
+    // .then(json=> console.log("testng json 2 => ", json))
+
+const a4 = () => fetch('../generated.json')
+    .then(res=>res.json())
+    // .then(json=> console.log("testng json 3 => ", json))
+
+Promise.all([a4(), a1, a2(), a3()]).then(value => console.log("testng value => ", value))
